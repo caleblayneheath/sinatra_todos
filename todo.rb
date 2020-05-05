f94a2c8f08cde832bc06ca031805e4beded36811
caleblayneheath <caleb.heath@gmail.com>
2020-05-05 12:52:56 -0400

    added add todos feature

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -38,15 +39,44 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# render existing list, based on position in array
+get "/lists/:id" do
+  @id = params[:id].to_i
+  @list = session[:lists][@id]
+
+  erb :list, layout: :layout
+end
+
+# return error message if name is invalid, nil if valid
+def error_for_list_name(name)
+  if !(1..100).cover?(name.size)
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
+end
+
 # create new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if (1..100).cover?(list_name.size)
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout  
+  else
     session[:lists] << {name: list_name, todos: []}
     session[:success] = "The list has been created."
     redirect "/lists"
-  else
-    session[:error] = "List name must be between 1 and 100 characters."
-    erb :new_list, layout: :layout
   end
+end
+
+# create todo
+post "/list/:id" do
+  todo_name = params[:todo_name]
+  
+  id = params[:id].to_i
+  list = session[:lists][id]
+  list[:todos] << todo_name
+
+  redirect "/lists/#{id}"
 end
